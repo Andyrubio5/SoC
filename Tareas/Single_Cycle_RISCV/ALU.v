027bb6282488e5913e0a0b3750db4bd3d6ee629d
Andy <a01645257@tec.mx>
2025-05-28 09:36:52 -0600

    updateALU

--- Tareas/Single_Cycle_RISCV/ALU.v
+++ Tareas/Single_Cycle_RISCV/ALU.v
@@ -1,4 +1,4 @@
-module ALU (
+module ALU(
     input [31:0] SrcA,
     input [31:0] SrcB,
     input [2:0] ALUControl,
@@ -12,14 +12,15 @@ module ALU (
             3'b001: ALUResult = SrcA - SrcB;      // Subtract
             3'b010: ALUResult = SrcA & SrcB;      // AND
             3'b011: ALUResult = SrcA | SrcB;      // OR
-            3'b101: ALUResult = (SrcA < SrcB) ? 32'd1 : 32'd0; // SLT
+            3'b101: ALUResult = ($signed(SrcA) < $signed(SrcB)) ? 32'd1 : 32'd0;
             default: ALUResult = 32'b0;
         endcase
 
-        if (ALUResult == 32'b0)
+        if (ALUResult == 32'b0) begin
             Zero = 1'b1;
-        else
+        end else begin
             Zero = 1'b0;
+        end
     end
 
 endmodule
